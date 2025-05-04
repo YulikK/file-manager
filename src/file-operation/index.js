0bc751148429e1856573de56e72b2c5fd58cfecf
YulikK <yulikpoch@gmail.com>
2025-05-04 15:55:14 +0200

    feat: add mkdir command to create directories

--- src/file-operation/index.js
+++ src/file-operation/index.js
@@ -1,5 +1,6 @@
 import cat from './commands/cat.js';
 import add from './commands/add.js';
+import mkdir from './commands/mkdir.js';
 
 export default class FileOperation {
   async cat(currentDir, args) {
@@ -8,5 +9,9 @@ export default class FileOperation {
 
   async add(currentDir, args) {
     return await add(currentDir, args);
+  }
+
+  async mkdir(currentDir, args) {
+    return await mkdir(currentDir, args);
   }
 }
