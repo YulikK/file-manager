071f011abfdf46c9cffb4db4f22895b2cc4f9903
YulikK <yulikpoch@gmail.com>
2025-05-05 21:51:14 +0200

    refactor(file-operation): simplify rm and mv commands using async/await

--- src/file-operation/commands/mv.js
+++ src/file-operation/commands/mv.js
@@ -1,45 +1,7 @@
-import fs from 'fs';
-import path from 'path';
-import { logWithColor, resolvePath } from '../../helper.js';
-import { COLORS_MAP } from '../../constants.js';
+import rn from './rn.js';
+import cp from './cp.js';
 
-export default function mv(currentDir, args) {
-  return new Promise((resolve) => {
-    try {
-      const sourcePath = resolvePath(currentDir, args[0]);
-      const targetPath = resolvePath(currentDir, args[1]);
-
-      const finalTargetPath =
-        fs.existsSync(targetPath) && fs.statSync(targetPath).isDirectory()
-          ? path.join(targetPath, path.basename(sourcePath))
-          : targetPath;
-
-      const readStream = fs.createReadStream(sourcePath);
-      const writeStream = fs.createWriteStream(finalTargetPath);
-
-      readStream.on('error', (error) => {
-        logWithColor(`Operation failed:${error}`, COLORS_MAP.RED);
-        resolve();
-      });
-
-      writeStream.on('error', (error) => {
-        logWithColor(`Operation failed:${error}`, COLORS_MAP.RED);
-        resolve();
-      });
-
-      writeStream.on('finish', () => {
-        fs.unlink(sourcePath, (error) => {
-          if (error) {
-            logWithColor(`Operation failed:${error}`, COLORS_MAP.RED);
-          }
-          resolve();
-        });
-      });
-
-      readStream.pipe(writeStream);
-    } catch (error) {
-      logWithColor(`Operation failed:${error}`, COLORS_MAP.RED);
-      resolve();
-    }
-  });
+export default async function mv(currentDir, args) {
+  await cp(currentDir, args);
+  await rn(currentDir, args);
 }
--- src/file-operation/commands/rm.js
+++ src/file-operation/commands/rm.js
@@ -1,21 +1,8 @@
-import fs from 'fs';
-import { logWithColor, resolvePath } from '../../helper.js';
-import { COLORS_MAP } from '../../constants.js';
+import fs from 'node:fs/promises';
+import { resolvePath } from '../../helper.js';
 
-export default function rm(currentDir, args) {
-  return new Promise((resolve) => {
-    try {
-      const filePath = resolvePath(currentDir, args[0]);
+export default async function rm(currentDir, args) {
+  const filePath = resolvePath(currentDir, args[0]);
 
-      fs.unlink(filePath, (error) => {
-        if (error) {
-          logWithColor(`Operation failed:${error}`, COLORS_MAP.RED);
-        }
-        resolve();
-      });
-    } catch (error) {
-      logWithColor(`Operation failed:${error}`, COLORS_MAP.RED);
-      resolve();
-    }
-  });
+  await fs.unlink(filePath);
 }
