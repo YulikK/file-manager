2c55864c9f09121e7687df825441aa4dd92f0a2e
YulikK <yulikpoch@gmail.com>
2025-05-05 22:19:27 +0200

    refactor: simplify hash function using pipeline

--- src/file-operation/commands/hash.js
+++ src/file-operation/commands/hash.js
@@ -1,32 +1,14 @@
 import fs from 'fs';
 import crypto from 'crypto';
-import { resolvePath, logWithColor } from '../../helper.js';
-import { COLORS_MAP } from '../../constants.js';
+import { pipeline } from 'node:stream/promises';
+import { stdout } from 'node:process';
+import { resolvePath } from '../../helper.js';
 
-export default function hash(currentDir, args) {
-  return new Promise((resolve) => {
-    try {
-      const filePath = resolvePath(currentDir, args[0]);
+export default async function hash(currentDir, args) {
+  const filePath = resolvePath(currentDir, args[0]);
+  const readStream = fs.createReadStream(filePath);
 
-      const readStream = fs.createReadStream(filePath);
-      const hash = crypto.createHash('sha256');
+  const hash = crypto.createHash('sha256');
 
-      readStream.on('error', (error) => {
-        logWithColor(`Operation failed:${error}`, COLORS_MAP.RED);
-        resolve();
-      });
-
-      readStream.on('data', (chunk) => {
-        hash.update(chunk);
-      });
-
-      readStream.on('end', () => {
-        logWithColor(hash.digest('hex'), COLORS_MAP.BLUE);
-        resolve();
-      });
-    } catch (error) {
-      logWithColor(`Operation failed:${error}`, COLORS_MAP.RED);
-      resolve();
-    }
-  });
+  await pipeline(readStream, hash.setEncoding('hex'), stdout, { end: false });
 }
